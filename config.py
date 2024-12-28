0a2cb38a79ff5ae41211d70d2e00dafc2d88e24d
ItsRrapidxD <147593105+ItsRrapidxD@users.noreply.github.com>
2024-12-28 11:17:49 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+
+# Bot
+API_KEY = "t-676fdd0f7d3a76ee44be346c-88e8ce639e284439b7443ea6"
+TOKEN = "acc token"
+
+# Litecoin (LTC) wallet details
+LTC_ADDRESS = "your_ltc_address"
+LTC_PRIVATE_KEY = "your_ltc_private_key"
+
+# Ethereum (ETH) wallet details
+ETH_ADDRESS = "your_eth_address"
+ETH_PRIVATE_KEY = "your_eth_private_key"
+
+# Solana (SOL) wallet details
+SOL_ADDRESS = "your_sol_address"
+SOL_PRIVATE_KEY = "your_sol_private_key"
+
+# Tether (USDT) wallet details
+USDT_ADDRESS = "your_usdt_address"
+USDT_PRIVATE_KEY = "your_usdt_private_key"
